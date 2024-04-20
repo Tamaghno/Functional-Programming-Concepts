56e109256cd00e18e5203e8173513101d3059787
Tamaghno Chaudhuri <tamaghno.chaudhuri@gmail.com>
2024-04-20 12:52:46 +0530

    # Chaining functions together

--- src/main/java/org/tamcodes/functionalInterface/_Function.java
+++ src/main/java/org/tamcodes/functionalInterface/_Function.java
@@ -10,6 +10,13 @@ public class _Function {
 
         System.out.println(incrementByOneFunction.apply(2));
 
+        System.out.println(incrementByOneFunction.andThen(multiplyBy10Function).apply(5)); //this is function chaining
+
+        //another way
+
+        Function<Integer, Integer> integerIntegerFunction = incrementByOneFunction.andThen(multiplyBy10Function);
+        System.out.println(integerIntegerFunction.apply(8));
+
     }
 
     static int increment(int num){
@@ -17,5 +24,7 @@ public class _Function {
     }
 
     static Function<Integer, Integer> incrementByOneFunction = num -> num+1; // this is a Function interface
+
+    static Function<Integer, Integer> multiplyBy10Function = num -> num * 10; // this is a Function interface
 
 }
